3f6c73348ed234024c1bb28778eb920e1bd8b87a
haithamkhadra <haithamkhadra@hotmail.com>
2021-08-28 20:14:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+// import { useEffect } from 'react';
+import Question from "./components/Question.js";
 
-function App() {
+const App = () => {
+  
+  const [amount, setAmount] = useState(10);
+  const [category, setCategory] = useState(10);
+  const [difficulty, setDifficulty] = useState('easy');
+  const [type, setType] = useState('multiple');
+  
+  const [counter, setCounter] = useState(0)
+  const [quizQs, setQuizQs] = useState('');
+  
+  const IncrementCounter = () => {
+    setCounter(counter + 1)
+  }
+
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  }
+  const handleCat = (e) => {
+    setCategory(e.target.value);
+  }
+  const handleType = (e) => {
+    setType(e.target.value);
+  }
+  const handleDif = (e) => {
+    setDifficulty(e.target.value);
+  }
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setAmount('');
+    setCategory('');
+    setType('');
+    setDifficulty('');
+
+    const getQuestions = async () => {
+      
+      const response = await fetch(`https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`)
+      const data = await response.json();
+      console.log(data);
+      setQuizQs(data);
+    }
+    getQuestions();
+  }
+
+  // const onSubmitAnswer = (e, id, a) => {
+  //   e.preventDefault();
+  //   // const ans = document.querySelector(`input[name="${ID}"]:checked`).value;
+  //   const form = document.getElementById(`${id}`);
+  //   console.log(form.elements[`${id}`]);
+  //   const ans = form.elements[`${id}`].value;
+  //   console.log(ans + '...' + a)
+  //   if (ans === a) {
+  //     // IncrementCounter();
+  //     console.log('correct');
+  //   } else {
+  //     console.log('incorrect');
+  //   }
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+        <form onSubmit={getSearch} className="search-form" action="" method="get">
+          <input className="search-bar" type="text" onChange={handleAmount} placeholder='Search Amount' value={amount} name="q"/>
+          <input className="search-bar" type="text" onChange={handleCat} placeholder='Search Cat' value={category} name="q"/>
+          <input className="search-bar" type="text" onChange={handleType} placeholder='Search Type' value={type} name="q"/>
+          <input className="search-bar" type="text" onChange={handleDif} placeholder='Search diff' value={difficulty} name="q"/>
+          <button className="search-button" type="submit">
+            filter quiz
+          </button>
+        </form> 
+
+        <h1>{counter}</h1>
+      
+        {quizQs.response_code === 0         
+        ? quizQs.results.map((qs, ID) => (
+            <Question 
+              key={ID} 
+              ID={ID} 
+              question={qs} 
+              IncrementCounter={IncrementCounter}
+              // onSubmitAnswer={onSubmitAnswer}
+              />
+            )) 
+            : <div>Click to Start</div>}
+
+      </div>
   );
 }
+
 
 export default App;

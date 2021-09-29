72756da1e03621f02f7f289d8095cd60ed8bb9cb
haithamkhadra <haithamkhadra@hotmail.com>
2021-09-29 22:02:31 +0300

    style question card

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,19 +1,18 @@
 import { useState, useEffect } from 'react';
 import React from "react";
 
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
+// import Radio from '@mui/material/Radio';
+// import RadioGroup from '@mui/material/RadioGroup';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import FormControl from '@mui/material/FormControl';
 
 
 const Question = props => {
 
-  const { question, ID, IncrementScore, IncrementClicksCounter } = props
+  const { question, IncrementScore, IncrementClicksCounter } = props;
 
-  const [hide, sethide] = useState('')
-  const [options, setOptions] = useState([])
+  const [hide, sethide] = useState('');
+  const [options, setOptions] = useState([]);
 
   const getRandomInt = max => {
     return Math.floor(Math.random() * Math.floor(max));
@@ -23,81 +22,85 @@ const Question = props => {
     if (!question) {
       return;
     }
-    let answers = [...question.incorrect_answers]
-    answers.splice(getRandomInt(question.incorrect_answers.length), 0, question.correct_answer)
+    let answers = [...question.incorrect_answers];
+    answers.splice(getRandomInt(question.incorrect_answers.length), 0, question.correct_answer);
 
-    setOptions(answers)
-  }, [question])
+    setOptions(answers);
+  }, [question]);
 
 
   const onSubmitAnswer = (e) => {
-    e.target.value === question.correct_answer
-      ? IncrementScore()
-      : console.log('incorrect');
+    // e.target.value === question.correct_answer
+    //   ? IncrementScore()
+    //   : console.log('incorrect');
+    console.log(e.target.dataset.value);
+    if (e.target.dataset.value === question.correct_answer) {
+      IncrementScore();
+      e.target.style.background = 'green';
+      console.log('if');
+    } else {
+      e.target.style.background = 'red';
+      console.log('else');
+    }
     setTimeout(() => {
       sethide('hide');
       IncrementClicksCounter();
     }, 2000);
   }
 
-  // const style = {
-  //   'position': 'absolute',
-  //   'top': '100px',
-  //   'left': '50px',
-  //   'backgroundColor': 'green',
-  //   'color': 'yellow',
-  //   'height': '300px',
-  //   'width': '500px'
-  // }
-
-
   if (question.question !== undefined) {
 
-    const renderQuestion = React.createElement("h2", { dangerouslySetInnerHTML: { __html: `${question.question}` } });
-    
+    const renderQuestion = React.createElement(
+      "h2",
+      {
+        className: "question",
+        dangerouslySetInnerHTML: { __html: `${question.question}` }
+      }
+    );
+
     return (
       <div className={`abs totest ${hide}`}>
-        <FormControl component="fieldset">
-
-        
         {renderQuestion}
 
-        {/* <form id={`${ID}`}>
-          <fieldset > */}
+        <form>
+          <fieldset >
             {
               options
-              .map((answer, key) => (
-                <div key={key} >
-                    <RadioGroup
-                      aria-label="gender"
-                      // name="radio-buttons-group"
+                .map((answer, key) => (
+                  <div key={key} >
+                    <div 
+                      className="classss" 
+                      onClick={onSubmitAnswer} 
+                      data-value={answer}
                     >
-                    {/* <input onClick={onSubmitAnswer} type="radio" value={answer} />   */}
-                      <FormControlLabel onClick={onSubmitAnswer} value={answer} control={<Radio />} label={`${answer}`} />
-                    {/* <FormLabel component="legend">{answer}</FormLabel> */}
-                    </RadioGroup>
+                      {answer}
+                    </div>
                   </div>
                 ))
-              }
-          {/* </fieldset>
-        </form> */}
-        </FormControl>
-
+            }
+          </fieldset>
+        </form>
       </div>
     )
   }
-  // if (question.question !== undefined) {
+}
+
+export default Question;
+
+
+
+// if (question.question !== undefined) {
   //   const renderQuestion = React.createElement("div", { dangerouslySetInnerHTML: { __html: `<div>${question.question}</div>` } });
   //   return (
-  //     <div style={style} className={`${hide}`}>
-        
-  //       {renderQuestion}
+//     <div style={style} className={`${hide}`}>
 
-  //       <form id={`${ID}`}>
-  //         <fieldset >
-  //           {
-  //             options
-  //               .map((answer, key) => (
+//       {renderQuestion}
+
+//       <form id={`${ID}`}>
+//         <fieldset >
+//           {
+//             options
+//               .map((answer, key) => (
   //                 <div key={key} >
   //                   <input onClick={onSubmitAnswer} type="radio" name={ID} value={answer} />
   //                   <label >{answer}</label>
@@ -105,12 +108,31 @@ const Question = props => {
   //               ))
   //           }
   //         </fieldset>
-  //       </form>
-  //     </div>
-  //   )
-  // }
+//       </form>
+//     </div>
+//   )
+// }
 
 
-}
 
-export default Question;
+/* <FormControl component="fieldset">
+  {renderQuestion}
+
+  {
+    options
+      .map((answer, key) => (
+        <div key={key}>
+          <RadioGroup aria-label="anwser">
+            {
+              <FormControlLabel
+                onClick={onSubmitAnswer}
+                value={answer}
+                control={<Radio />}
+                label={`${answer}`}
+              />
+            }
+          </RadioGroup>
+        </div>
+      ))
+  }
+</FormControl> */
